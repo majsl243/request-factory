4ad6386fa86fdfc12a245ac212726087527f18df
Majdi <majdilada@gmail.com>
2022-11-07 20:24:44 +0200

    Added support to use array of Records with factory decorator

--- src/FactoryDecorator.ts
+++ src/FactoryDecorator.ts
@@ -1,7 +1,8 @@
 import { FactoryValue, FactoryValueGenerator } from "./interfaces/FactoryValue";
 import { FactoryStorage } from "./MetadataStorage";
 
-export function Factory(arg: FactoryValue | FactoryValueGenerator | Record<string, FactoryValue>) {
+export function Factory(
+	arg: FactoryValue | FactoryValueGenerator | Record<string, FactoryValue> | Array<Record<string, FactoryValue>>) {
 	return (target: Record<string, any>, propertyKey: string) => {
 		FactoryStorage.addPropertyMeta ({
 			target: target.constructor,
--- src/interfaces/PropertyMeta.ts
+++ src/interfaces/PropertyMeta.ts
@@ -3,5 +3,5 @@ import { FactoryValue, FactoryValueGenerator } from "./FactoryValue";
 export interface PropertyMeta {
     target: Function
     propertyKey: string
-    arg: FactoryValue | FactoryValueGenerator | Record<string, FactoryValue>   
+    arg: FactoryValue | FactoryValueGenerator | Record<string, FactoryValue> | Array<Record<string, FactoryValue>>
 }
